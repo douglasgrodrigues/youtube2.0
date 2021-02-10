38a6ab1e7520edd5699368ca70a9bdf6cb597ac4
Douglas Ghiraldello Rodrigues <d.roodrigues-rs@hotmail.com>
2021-02-10 19:40:30 -0300

    Consumindo API do youtube, exibindo resultados com o Retrofit

--- app/src/main/java/douglasghiraldello/cursoandroid/youtubeapp/helper/YoutubeConfig.java
+++ app/src/main/java/douglasghiraldello/cursoandroid/youtubeapp/helper/YoutubeConfig.java
@@ -5,4 +5,6 @@ public class YoutubeConfig {
     public static String CANAL_ID = "UCz2zerpoRHtO3r2uKFAtcPg";
     public static String CHAVE_YOUTUBE_API = "AIzaSyASLampna1cmU5ZEsQ0DPRen-vM7Yu3K2k";
     public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
+
+    //
 }
