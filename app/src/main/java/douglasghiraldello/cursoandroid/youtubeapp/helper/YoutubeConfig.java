c428fba8ab0afd7f96ac8c6deb6d4ec8ea39efd1
Douglas Ghiraldello Rodrigues <60372253+douglasgrodrigues@users.noreply.github.com>
2021-02-10 20:08:06 -0300

    Update YoutubeConfig.java

--- app/src/main/java/douglasghiraldello/cursoandroid/youtubeapp/helper/YoutubeConfig.java
+++ app/src/main/java/douglasghiraldello/cursoandroid/youtubeapp/helper/YoutubeConfig.java
@@ -2,8 +2,8 @@ package douglasghiraldello.cursoandroid.youtubeapp.helper;
 
 public class YoutubeConfig {
 
-    public static String CANAL_ID = "UCz2zerpoRHtO3r2uKFAtcPg";
-    public static String CHAVE_YOUTUBE_API = "AIzaSyASLampna1cmU5ZEsQ0DPRen-vM7Yu3K2k";
+    public static String CANAL_ID = "##";
+    public static String CHAVE_YOUTUBE_API = "##";
     public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
 
     //
